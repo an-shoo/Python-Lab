f9d031abdef893af009db2bfa58e967210f47867
Abinash <abinash0949@gmail.com>
2024-07-03 02:51:02 +0530

    Delete 1b.py

--- 1b.py
+++
@@ -1,8 +0,0 @@
-def sort_tuples_by_length(strings):
-    tuple_list = [(s, len(s)) for s in strings]
-    sorted_list = sorted(tuple_list, key=lambda x: x[1])
-    return sorted_list
-
-strings = ["apple", "banana", "cherry", "date"]
-sorted_tuples = sort_tuples_by_length(strings)
-print(sorted_tuples)
