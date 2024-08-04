35b71c1e682701b52dea19ba2d842d31913ae135
Abinash <abinash0949@gmail.com>
2024-08-04 23:53:59 +0530

    Update 3b.py

--- 3b.py
+++ 3b.py
@@ -1,18 +1,22 @@
-def find_max_min(arr):
-    max_num = max(arr)
-    min_num = min(arr)
-    return max_num, min_num
-
-def find_second_largest(arr):
-    unique_arr = list(set(arr))
-    unique_arr.remove(max(unique_arr))
-    second_largest = max(unique_arr)
-    return second_largest
-
-array = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5]
-max_num, min_num = find_max_min(array)
-second_largest = find_second_largest(array)
-
-print(f"Maximum number: {max_num}")
-print(f"Minimum number: {min_num}")
-print(f"Second largest number: {second_largest}")
+def maxi(l):
+    a=l[0]
+    for i in range(len(l)):
+        if a<l[i]:
+            a=l[i]
+    return a
+def mini(l):
+    a=l[0]
+    for i in range(len(l)):
+        if a>l[i]:
+            a=l[i]
+    return a
+def second(l):
+    m=maxi(l)
+    l.remove(m)
+    s=maxi(l)
+    return s
+l=[1,3,4,67,2]
+m=maxi(l)
+n=mini(l)
+q=second(l)
+print(m,n,q)
